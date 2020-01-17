4f1dcf773091f7d6a324a0a38c4e36da5adff4ef
Jason Lau <i@dotkrnl.com>
2020-01-16 16:39:50 -0800

    Fix typo: cascade -> coalesced.

--- experiments/rgb2yuv/testbed.cpp
+++ experiments/rgb2yuv/testbed.cpp
@@ -15,33 +15,33 @@ void testbed(
 
     int effective_input = input_size / 3 / 16 * 16;
     int effective_output = effective_input;
-    int cascade = effective_input / 16;
+    int coalesced = effective_input / 16;
 
     int effective_size = effective_input * 3;
-    int cascade_size = effective_size / 16;
+    int coalesced_size = effective_size / 16;
     *output_size = effective_size;
 
-    ap_int<512> input_cascade[cascade_size];
-    ap_int<512> output_cascade[cascade_size];
+    ap_int<512> input_coalesced[coalesced_size];
+    ap_int<512> output_coalesced[coalesced_size];
 
-    for (int i = 0; i < cascade_size; i++) {
+    for (int i = 0; i < coalesced_size; i++) {
         for (int j = 0; j < 16; j++) {
             int cast_input = input[i * 16 + j];
-            input_cascade[i](32 * (j+1) - 1, 32 * j) = cast_input;
+            input_coalesced[i](32 * (j+1) - 1, 32 * j) = cast_input;
         }
     }
 
-    rgb2yuv(input_cascade,
-            input_cascade + cascade,
-            input_cascade + cascade * 2,
-            output_cascade,
-            output_cascade + cascade,
-            output_cascade + cascade * 2,
+    rgb2yuv(input_coalesced,
+            input_coalesced + coalesced,
+            input_coalesced + coalesced * 2,
+            output_coalesced,
+            output_coalesced + coalesced,
+            output_coalesced + coalesced * 2,
             effective_input);
 
-    for (int i = 0; i < cascade_size; i++) {
+    for (int i = 0; i < coalesced_size; i++) {
         for (int j = 0; j < 16; j++) {
-            int cast_output = output_cascade[i](32 * (j+1) - 1, 32 * j);
+            int cast_output = output_coalesced[i](32 * (j+1) - 1, 32 * j);
             output[i * 16 + j] = *((float *)&cast_output);
         }
     }
